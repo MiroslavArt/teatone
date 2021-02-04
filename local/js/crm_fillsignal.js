e8ef890f50ecd2c131640f528a451d54f36c83dc
Miros <lya-job@yandex.ru>
2021-02-04 08:25:15 +0300

    контроллер для вывода статусов маячков

--- local/js/crm_fillsignal.js
+++ local/js/crm_fillsignal.js
@@ -34,16 +34,74 @@ BX.iTrack.Crm.FillSignal = {
     kanbanHandler: function(grid){
         this.kanban = grid;
         console.log(this.kanban)
+        var collectSignals = []
+        for(var i in grid.items) {
+            if(i>0) {
+            //if(grid.items[i].data.hasOwnProperty('phone')) {
+            //    if(grid.items[i].data.phone.hasOwnProperty('contact')) {
+            //        if (grid.items[i].data.phone.contact.length) {
+            //            var localValue = localStorage.getItem(grid.items[i].data.phone.contact[0].value);
+            //            if (localValue == null) {
+                            collectSignals.push(i);
+            //            }
+            //        }
+            //    }
+            //}
+            }
+        }
+        console.log(collectSignals)
+        if(collectSignals.length) {
+            this.requestSignals(collectSignals).then(function(response) {
+                console.log(response);
+                //this.processCollectionResponse(response);
+                //this.processKanbanSignals();
+            }.bind(this), function(error){
+                console.log(error);
+            }.bind(this));
+        } else {
+            //this.processKanbanSignals();
+        }
     },
     listHandler: function(grid){
         this.list = grid;
         console.log(this.list)
     },
-    requestSignals: function(phone) {
+    requestSignals: function(signal) {
         return BX.ajax.runAction('itrack:custom.api.signal.getSignal', {
             data: {
-                phone: phone
+                signals: signal
             }
         });
     },
+    processCollectionResponse: function(response) {
+        console.log(response);
+        if(response.hasOwnProperty('status')) {
+            if(response.status == 'success') {
+                if(response.data.length) {
+                    for(var i in response.data) {
+                        localStorage.setItem(response.data[i].phone, response.data[i].timezone);
+                    }
+                }
+            }
+        }
+    },
+    processKanbanSignals: function() {
+        var items = this.kanban.items;
+        for(var i in items) {
+            if(items[i].data.hasOwnProperty('phone')) {
+                if(items[i].data.phone.hasOwnProperty('contact')) {
+                    if (items[i].data.phone.contact.length) {
+                        var localValue = localStorage.getItem(items[i].data.phone.contact[0].value);
+                        if (localValue !== null) {
+                            if (!items[i].contactBlock.querySelector('.itrack-custom-crm-phonetime__phone-block')) {
+                                var timeNode = this.createTimeNodeForContactBlock();
+                                BX.append(timeNode, items[i].contactBlock);
+                                new BX.iTrack.Crm.PhoneTimezone.Timer(localValue, timeNode);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- local/modules/itrack.custom/lib/controller/signal.php
+++ local/modules/itrack.custom/lib/controller/signal.php
@@ -1,14 +1,39 @@
 <?php
 namespace iTrack\Custom\Controller;
 
+use Bitrix\Main;
+use Bitrix\Crm;
 use Bitrix\Main\Engine\Controller;
 
 class Signal extends Controller
 {
-    public function getSignalAction($phone)
+    public function getSignalAction($signals)
     {
-        $phoneHelper = new PhoneHelper($phone);
-        return $phoneHelper->getTimezone();
+        //$phoneHelper = new PhoneHelper($phone);
+        \Bitrix\Main\Diag\Debug::writeToFile($signals, "signals", "__miros.log");
+        if (\Bitrix\Main\Loader::includeModule("crm")) {
+            foreach ($signals as $signal) {
+                $arFilterDeal = array('ID'=>$signal);
+                // тут заменить коды полей при переносе на бой
+                $arSelectD = array('ID', 'UF_CRM_1612334271');
+                $obResDeal = \CCrmDeal::GetListEx(false,$arFilterDeal,false,false,$arSelectD)->Fetch();
+                if($obResDeal['UF_CRM_1612334271']==116) {
+                    $signalarr[$signal] = 'Client_in_1C';
+                } elseif ($obResDeal['UF_CRM_1612334271']==117) {
+                    $signalarr[$signal] = 'Order_in_1C';
+                } elseif ($obResDeal['UF_CRM_1612334271']==118) {
+                    $signalarr[$signal] = 'Doc_mistake';
+                } elseif ($obResDeal['UF_CRM_1612334271']==120) {
+                    $signalarr[$signal] = 'Scan_exists';
+                } elseif ($obResDeal['UF_CRM_1612334271']==121) {
+                    $signalarr[$signal] = 'No_docs';
+                } elseif ($obResDeal['UF_CRM_1612334271']=="") {
+                    $signalarr[$signal] = 'Empty';
+                }
+            }
+        }
+        //$signalarr[17] = '1C';
+        return $signalarr;
     }
 
 
