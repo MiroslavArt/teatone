867d9ae07e96c2b1ad974b24e3df249880240d20
Miros <lya-job@yandex.ru>
2021-06-16 11:56:50 +0300

    обработчик дублей

--- local/modules/itrack.custom/lib/agent/exchange.php
+++ local/modules/itrack.custom/lib/agent/exchange.php
@@ -22,4 +22,37 @@ class Exchange
 
         return '\iTrack\Custom\Agent\Exchange::import1c('.$type.');';
     }
+
+    public static function cleanduplicates()
+    {
+        \CModule::IncludeModule("iblock");
+        $iblockid = 20;
+        $arFilter = Array(
+            "IBLOCK_ID"=>$iblockid,
+            "ACTIVE"=>"Y"
+        );
+        $res = \CIBlockElement::GetList(Array(), $arFilter, Array("CODE"));
+        while($ar_fields = $res->GetNext())
+        {
+            if($ar_fields['CNT']>1) {
+                $arFilter = Array(
+                    "IBLOCK_ID"=>$iblockid,
+                    "CODE"=>$ar_fields['CODE']
+                );
+                $res2 = \CIBlockElement::GetList(Array("ÏD"=>DESC), $arFilter, false, false, ["ÏD"]);
+
+                $maxcount = $ar_fields['CNT'];
+                $curcount = 1;
+
+                while($ar_fields2 = $res2->GetNext())
+                {
+                    if($curcount>1 && $curcount<=$maxcount) {
+                        echo \CIBlockElement::Delete($ar_fields2["ID"]);
+                    }
+                    $curcount++;
+                }
+            }
+        }
+        return '\iTrack\Custom\Agent\Exchange::cleanduplicates();';
+    }
 }
